86047c6a758b2e8260f67ee5b7e371692f1ab33e
maxlapru <maxim@lapkovsky.ru>
2023-03-24 14:50:09 +0700

    maxlap komp - 24.03.23 14:50

--- src/main/java/ChessBoardAttackConsole.java
+++ src/main/java/ChessBoardAttackConsole.java
@@ -13,6 +13,10 @@ public class ChessBoardAttackConsole {
     private static int countFiguresOnBoard;
     private static int countAtTemps;
     private static final char[] FIGURES = {'K','Q','B','B','N','N','R','R','P','P','P','P','P','P','P','P'};
+
+    private static final int[][] POINTERS = {{-1,-2},{-2,-1},{-2,1},{-1,2},{1,2},{2,1},{2,1},{1,2},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1},{1,1},{1,-1}};
+    private static final char[] = POINTERS_FIGURES {'N','N','N','N','N','N','N','N','K','K','K','K','K','K','K','K','P','P'};
+
     private static final char DOT_EMPTY = '*';
     private static final char DOT_OPEN = ' ';
     private static final boolean DEBUG_MODE = false;
